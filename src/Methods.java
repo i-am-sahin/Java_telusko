c90d5b6d0cf3eff5345362d24f0f5ec2060353cb
Sahin Islam <sahinislam878@.com>
2025-04-07 03:09:14 +0530

    Progress: Completed Method Lecture form UDemy

---
+++ src/Methods.java
@@ -0,0 +1,22 @@
+class Computer{
+    public void playMusic(){
+        System.out.println("Playing Music....");
+    }
+    public String getPen(int cost){
+        if (cost >= 10) return "Pen";
+        return "Nothing.....";
+    }
+}
+
+public class Methods {
+    public static void main(String[] args) {
+
+        //Creating Computer Object
+        Computer obj = new Computer();
+        //Class_name reference_name = new_keyword againClassName;
+        obj.playMusic();
+        String getPen = obj.getPen(15);
+        System.out.println(getPen);
+
+    }
+}
