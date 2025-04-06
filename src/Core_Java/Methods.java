465f9209151476f5626821554c3b9d0520647f56
Sahin Islam <sahinislam878@.com>
2025-04-07 03:11:03 +0530

    File: create a Packege called Core_java

--- src/Core_Java/Methods.java
+++ src/Core_Java/Methods.java
@@ -1,3 +1,5 @@
+package Core_Java;
+
 class Computer{
     public void playMusic(){
         System.out.println("Playing Music....");
@@ -11,7 +13,7 @@ class Computer{
 public class Methods {
     public static void main(String[] args) {
 
-        //Creating Computer Object
+        //Creating Core_Java.Computer Object
         Computer obj = new Computer();
         //Class_name reference_name = new_keyword againClassName;
         obj.playMusic();
