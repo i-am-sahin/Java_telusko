977d7578b76dd240cab4d191c3d233200ef16aaf
Sahin Islam <sahinislam878@.com>
2025-04-17 02:27:49 +0530

    Progress: Completed 2D Array Lecture form UDemy

--- src/Core_Java/Arrays.java
+++ src/Core_Java/Arrays.java
@@ -11,5 +11,42 @@ public class Arrays {
         for (int i = 0; i < n;i++){
             System.out.println(nums[i]);
         }
+
+        //2D Array
+        int arr[][] = new int[3][4];
+        for (int i = 0; i < 3;i++){
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+
+
+        }
+
+        //Adding values in the array
+
+        for (int i = 0; i < 3;i++){
+
+            for (int j = 0; j < 4; j++) {
+                int random = (int)(Math.random()*100);
+                arr[i][j] = random;
+            }
+
+        }
+        for (int i = 0; i < 3;i++){
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+        //Enhanced For loop
+        for(int x[]:arr){
+            for (int m:x){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
