21dd4b3f915f41666881c4972d566c5ee41ab47e
Sahin Islam <sahinislam878@.com>
2025-05-10 14:21:23 +0530

    Progress: Completed Static Method Lecture form Udemy

--- src/Core_Java/Static_key.java
+++ src/Core_Java/Static_key.java
@@ -6,6 +6,16 @@ class Mobile{
     public void show(){
         System.out.println(Brand + " : " + Price + " : " + Name);
     }
+//    public static void show1(){
+////        System.out.println("It's a Static Method");
+//        System.out.println(Brand + " : " + Price + " : " + Name); //It's Gives Error We Can not use non Static variable inside a Static Method.
+//    }
+
+    public static void show1(Mobile obj){
+    //        System.out.println("It's a Static Method");
+        System.out.println(obj.Brand + " : " + obj.Price + " : " + Name); //It's Gives Error We Can not use non Static variable inside a Static Method.
+    }
+
 }
 public class Static_key {
     public static void main(String[] args) {
@@ -22,5 +32,6 @@ public class Static_key {
 
         obj1.show();
         obj2.show();
+        Mobile.show1(obj1);
     }
 }
