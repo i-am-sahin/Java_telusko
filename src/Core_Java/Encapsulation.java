f83f584db91aa6830e87770c6ce329b8375ef66b
Sahin Islam <sahinislam878@.com>
2025-05-11 10:27:12 +0530

    Progress: Completed Encapsulation Lecture form Udemy

---
+++ src/Core_Java/Encapsulation.java
@@ -0,0 +1,36 @@
+package Core_Java;
+
+class Human{
+    private String Name;
+    private int age;
+
+    //With the help of getName and getAge method we can access the private instance variable,Because getName and getAge, Name and Age belongs from same class.
+    public String getName(){
+        return Name;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String n){
+        Name = n;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Human obj = new Human();
+
+
+//        We are making your variable Private and the only way to access them with the Methods,So Basically we're binding our data with the methods ,notone outside the class can directly access the variables Directly. -- Here We are encapsulate Data and Methods and that is Encapsulation :) .
+
+        obj.setAge(21);
+        obj.setName("Sahin");
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName());
+
+
+    }
+}
