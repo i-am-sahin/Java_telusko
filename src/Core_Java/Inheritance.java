ebf0f9ad6ef4052726c8b59424b88362a2d88a0a
Sahin Islam <sahinislam878@gmail.com>
2025-05-17 00:48:49 +0530

    Progress: Completed Single and MultiLevel Inheritance Lecture from Udemy

--- src/Core_Java/Inheritance.java
+++ src/Core_Java/Inheritance.java
@@ -17,13 +17,21 @@ class AdvCalc extends Calc{
     }
 }
 
+//Multilevel Inheritance
+class VeryAdvCalc extends AdvCalc{
+    public int power(int n1,int n2){
+        return (int)Math.pow(n1,n2);
+    }
+}
+
 public class Inheritance {
     public static void main(String[] args) {
-        AdvCalc obj1 = new AdvCalc();
+        VeryAdvCalc obj1 = new VeryAdvCalc();
         int r1 = obj1.addition(4,5);
         int r2 = obj1.subtraction(5,8);
         int r3 = obj1.multiplication(5,8);
         int r4 = obj1.division(5,8);
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        int r5 = obj1.power(2,5);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
