1a8b9dab14bb596347dd7775b5c876276f2ca4f0
Sahin Islam <sahinislam878@gmail.com>
2025-06-15 21:24:02 +0530

    Progress: Completed Try with multiple Catch Lecture from Udemy

--- src/AdvanceJava/ExceptionHandling.java
+++ src/AdvanceJava/ExceptionHandling.java
@@ -2,14 +2,25 @@ package AdvanceJava;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
-        int i = 4;
+        int i = 1;
         int j = 0;
+        int arr[] = new int[5];
+        String str = null;
 
         try{
             //IF there is any problem in this block then it's not executed,Catch block will.
             j = 18/i;
-        }catch (Exception e){
-            System.out.println("Something Went Wrong.");
+            System.out.println(str.length());
+            System.out.println(arr[2]);
+            System.out.println(arr[5]);
+        }catch (ArithmeticException e){
+            System.out.println("Can't Divide with 0");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Stay in your Limit");
+        }
+        catch (Exception e){
+            System.out.println("Somthing Went Wrong....."  + e);
         }
         System.out.println(j);
 
