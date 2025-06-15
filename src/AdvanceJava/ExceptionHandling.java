5bbfaf64f447db130f87aa2fb96ceb52e3da67da
Sahin Islam <sahinislam878@gmail.com>
2025-06-15 21:42:33 +0530

    Progress: Completed Exception Throw Keyword Lecture from Udemy

--- src/AdvanceJava/ExceptionHandling.java
+++ src/AdvanceJava/ExceptionHandling.java
@@ -2,23 +2,21 @@ package AdvanceJava;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
-        int i = 1;
+        int i = 20;
         int j = 0;
-        int arr[] = new int[5];
-        String str = null;
+
 
         try{
             //IF there is any problem in this block then it's not executed,Catch block will.
             j = 18/i;
-            System.out.println(str.length());
-            System.out.println(arr[2]);
-            System.out.println(arr[5]);
+            if (j == 0)
+                throw new ArithmeticException("I don't want to do print zero"); //Here I create a ArithmeticException, and explicitly throwing a error here
+
         }catch (ArithmeticException e){
-            System.out.println("Can't Divide with 0");
+            j = 18/1;
+            System.out.println("That's the default output." + e);
         }
-        catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Stay in your Limit");
-        }
+
         catch (Exception e){
             System.out.println("Somthing Went Wrong....."  + e);
         }
