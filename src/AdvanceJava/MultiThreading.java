0fe8cd179ced87682e888afcadb8b9728d7a2d71
Sahin Islam <sahinislam878@gmail.com>
2025-06-29 01:12:23 +0530

    Progress: Completed Thread Priority and Sleep Lecture from Udemy

--- src/AdvanceJava/MultiThreading.java
+++ src/AdvanceJava/MultiThreading.java
@@ -3,6 +3,11 @@ class Show1 extends Thread{
     public void run(){
         for (int i = 0; i < 100;i++){
             System.out.println("Hii!!!");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -10,6 +15,11 @@ class Show2 extends Thread{
     public void run(){
         for (int i = 0; i < 100;i++){
             System.out.println("Hello!!!");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -17,6 +27,8 @@ public class MultiThreading {
     public static void main(String[] args) {
         Show1 obj1 = new Show1();
         Show2 obj2 = new Show2();
+        System.out.println(obj1.getPriority());
+        obj1.setPriority(Thread.MAX_PRIORITY);
         obj1.start();
         obj2.start();
     }
