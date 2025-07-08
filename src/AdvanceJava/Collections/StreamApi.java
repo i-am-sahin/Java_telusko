7cb69de2ae50961498ebc9739259afd37376367a
Sahin Islam <sahinislam878@gmail.com>
2025-07-08 12:09:02 +0530

    Progress: Completed Need Of Stream API Lecture from Udemy

---
+++ src/AdvanceJava/Collections/StreamApi.java
@@ -0,0 +1,35 @@
+package AdvanceJava.Collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamApi {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(4,5,7,3,2,6);
+
+
+
+//        for (int i = 0; i < nums.size();i++) {
+//            System.out.println(nums.get(i));
+//        }
+
+
+//        for(int n : nums){
+//            System.out.println(n);
+//        }
+
+        nums.forEach(n -> System.out.println(n));
+
+
+
+
+//        int sum = 0;
+//        for(int n : nums){
+//            if (n % 2 == 0){
+//                n = n * 2;
+//                sum += n;
+//            }
+//        }
+//        System.out.println(sum);
+    }
+}
