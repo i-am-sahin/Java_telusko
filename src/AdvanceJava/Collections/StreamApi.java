7354f6f81bef8a359cb11fed2c3b145574dac996
Sahin Islam <sahinislam878@gmail.com>
2025-07-08 13:17:02 +0530

    Progress: Completed StreamAPI Lecture from Udemy

--- src/AdvanceJava/Collections/StreamApi.java
+++ src/AdvanceJava/Collections/StreamApi.java
@@ -4,6 +4,7 @@ import java.lang.constant.Constable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class StreamApi {
     public static void main(String[] args) {
@@ -24,10 +25,27 @@ public class StreamApi {
 //        Consumer<Integer> cons = (Integer n) -> System.out.println(n);
 
 
+//        Stream<Integer> s1 = nums.stream();
+//        Stream<Integer> s2 = s1.filter(n -> n%2 == 0);
+//        Stream<Integer> s3 = s2.map(n -> n* 2);
+//
+//        int result = s3.reduce(0,(c,e) ->c+e);
+
+      int result =   nums.stream()
+                        .filter(n -> n%2 == 0)
+                                .map(n -> n * 2)
+                                        .reduce(0,(c,e) -> c + e);
+
+        System.out.println(result);
 
 
-        nums.forEach(n -> System.out.println(n));
+//        s3.forEach(n -> System.out.println(n));
 
+//        s1.forEach(n -> System.out.println(n));
+//Exception in thread "main" java.lang.IllegalStateException: stream has already been operated upon or closed
+//	at java.base/java.util.stream.AbstractPipeline.sourceStageSpliterator(AbstractPipeline.java:311)
+//	at java.base/java.util.stream.ReferencePipeline$Head.forEach(ReferencePipeline.java:807)
+//	at AdvanceJava.Collections.StreamApi.main(StreamApi.java:32)
 
 
 
